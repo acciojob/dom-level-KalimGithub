688db2a2a77ca6fd0eedfa4c5836f2161c218c37
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-08-22 01:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const level = document.getElementById("level");
+
+var n = level.textContent.split(" ");
+
+alert("The level of the element is: " + n[n.length-1] );
